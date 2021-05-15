17e962616bf946983aff38862fb8ebd0f43c64a5
Marcos <marcos.mendonca@matera.com>
2021-05-15 13:34:59 -0300

    Project created

---
+++ src/main/java/com/mvam/spperson/SppersonApplication.java
@@ -0,0 +1,13 @@
+package com.mvam.spperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SppersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SppersonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvam/spperson/SppersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvam.spperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SppersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
