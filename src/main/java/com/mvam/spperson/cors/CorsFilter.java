7df74f06b78de8aa6dc6072c99e1585539061ade
Marcos <marcos.mendonca@matera.com>
2021-07-13 18:34:48 -0300

    CORS

---
+++ src/main/java/com/mvam/spperson/cors/CorsFilter.java
@@ -0,0 +1,50 @@
+package com.mvam.spperson.cors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+    @Value("${allowed-origin}")
+    private String allowedOrigin;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        //estes Headers precisam estar na resposta
+        response.setHeader("Access-Control-Allow-Origin", allowedOrigin);
+        response.setHeader("Access-Control-Allow-Credentials", "true"); //true para permitir o envio do cookie
+
+        if("OPTIONS".equals(request.getMethod()) && allowedOrigin.equals(request.getHeader("Origin"))) {
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+            response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        //Not necessary
+    }
+
+    @Override
+    public void destroy() {
+        //Not necessary
+    }
+}
